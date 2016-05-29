0929dbed52c5a80afc0f5b312832c1e320fa14a9
despair1 <eveguide0@gmail.com>
2016-05-29 13:29:49 +0500

    view figure

--- Assets/scripts/figure.cs
+++ Assets/scripts/figure.cs
@@ -6,30 +6,50 @@ public class figure  {
 	public class figure_desc
     {
         private  int[,] pos;
-        public readonly int[] left_bound;
-        public readonly int[] right_bound;
+        private readonly int down_offset;
+        private readonly int left_offset;
         public readonly int[][,] contact_points;
         //public figure_desc(int[,] pos1, int[] lb, int[] rb, int[][,] cp)  {
         public figure_desc(int[,] pos1)
         {
             pos = pos1;
+            down_offset = get_down_offset();
+            left_offset = get_left_offset();
             //left_bound = lb;
             //right_bound = rb;
             //contact_points = cp;
         }
         int get_down_offset()
         {
+            int result = pos[0, 1];
+            for (int i=0; i < pos.GetLength(0); i++)
+            {
+                var a = pos[i, 1];
+                if (a < result) result = a;
+            }
             //foreach ( int[] y in pos)
-            return 1;
+            return result ;
+        }
+        int get_left_offset()
+        {
+            int result = pos[0, 0];
+            for (int i = 0; i < pos.GetLength(0); i++)
+            {
+                var a = pos[i, 0];
+                if (a < result) result = a;
+            }
+            //foreach ( int[] y in pos)
+            return result;
         }
         public void test()
         {
             for ( int i = 0; i < pos.GetLength(0); i++)
             {
-                Debug.Log("y " + pos[i,1]);
+                Debug.Log("y1 " + pos[i,1]);
             }
             
         }
+        public int[,] get_pos() { return (int [,]) pos.Clone(); }
     }
     public static figure_desc get_figure_desc(int num)
     {
--- Assets/scripts/quad.cs
+++ Assets/scripts/quad.cs
@@ -5,9 +5,11 @@ using System.Collections.Generic;
 public class quad :MonoBehaviour  {
     public GameObject quad_prefab;
     private List<GameObject> quads = new List<GameObject>();
+    int xpos=0;
+    int ypos=4;
 	// Use this for initialization
 	void Start () {
-        for (int i = 0; i < 10; i++)
+        /*for (int i = 0; i < 10; i++)
         {
             //Vector3 pos = new Vector3(i * 2 - 4, 2, 0);
             Vector3 pos = new Vector3((int)Random.Range(-4,4.9f), (int) Random.Range(-4,2.9f), 0);
@@ -15,8 +17,16 @@ public class quad :MonoBehaviour  {
             quads.Add(q);
             
             
-        }
+        }*/
         var a = figure.get_figure_desc(1);
+        var b = a.get_pos();
+        for( int i = 0; i < b.GetLength(0); i++)
+        {
+            Vector3 pos = new Vector3(xpos + b[i, 0], ypos + b[i, 1], 0);
+            GameObject q = (GameObject)Instantiate(quad_prefab, pos, Quaternion.identity);
+            quads.Add(q);
+        }
+        b[0, 1] = -4;
         a.test();
     }
 	
