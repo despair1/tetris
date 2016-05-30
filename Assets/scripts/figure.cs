2ae879bbf851580e4c3cc2d1ca2852e08a9f3bde
despair1 <eveguide0@gmail.com>
2016-05-30 13:51:34 +0500

    fixed rotation bug

--- Assets/scripts/figure.cs
+++ Assets/scripts/figure.cs
@@ -6,6 +6,8 @@ public class figure  {
 	public class figure_desc
     {
         private  int[,] pos;
+        private int[,,] positions; // quad num/xy/rotation num
+        private int rotation_num = 0;
         private readonly int down_offset;
         private readonly int left_offset;
         public readonly int[][,] contact_points;
@@ -13,6 +15,12 @@ public class figure  {
         public figure_desc(int[,] pos1)
         {
             pos = pos1;
+            positions = new int[pos1.GetLength(0), 2, 4];
+            set_rotation();
+            for (int rot = 0; rot < 4; rot++)
+            {
+
+            }
             down_offset = get_down_offset();
             left_offset = get_left_offset();
             //left_bound = lb;
@@ -49,7 +57,43 @@ public class figure  {
             }
             
         }
-        public int[,] get_pos() { return (int [,]) pos.Clone(); }
+        public int[,] get_pos() {
+            var result = new int[pos.GetLength(0), 2];
+            for (int i = 0; i < pos.GetLength(0); i++)
+            {
+                result[i,0]=positions[i, 0, rotation_num] ;
+                result[i,1]=positions[i, 1, rotation_num];
+
+            }
+            return result;
+        }
+        public int[,] left()
+        {
+            rotation_num++;
+            if (rotation_num >= 4) rotation_num = 0;
+            return get_pos();
+        }
+        private void set_rotation()
+        {
+            
+            for (int rot = 0; rot < 4; rot++)
+            {
+                var result = new int[pos.GetLength(0), 2];
+                for (int i = 0; i < pos.GetLength(0); i++)
+                {
+                    result[i, 0] = -pos[i, 1];
+                    result[i, 1] = pos[i, 0];
+                }
+                
+                for (int i = 0; i < pos.GetLength(0); i++)
+                {
+                    positions[i, 0, rot] = pos[i, 0];
+                    positions[i, 1, rot] = pos[i, 1];
+                }
+                pos = result;
+            }
+            //return result;
+        }
     }
     public static figure_desc get_figure_desc(int num)
     {
@@ -58,7 +102,7 @@ public class figure  {
         if ( true)
         {
             //a = new figure_desc[1];
-            int[,] pos2 = new int[,] { { 0, 0 }, { 0, 1 }, { 0, 2 } };
+            int[,] pos2 = new int[,] { { 0, 0 }, { 0, 1 }, { 0, 2 }, { 1, 0 } };
             var a = new figure_desc(pos2);
             return a;
         }
--- Assets/scripts/quad.cs
+++ Assets/scripts/quad.cs
@@ -5,7 +5,7 @@ using System.Collections.Generic;
 public class quad :MonoBehaviour  {
     public GameObject quad_prefab;
     private List<GameObject> quads = new List<GameObject>();
-    int xpos=0;
+    int xpos=-8;
     int ypos=4;
 	// Use this for initialization
 	void Start () {
@@ -20,14 +20,22 @@ public class quad :MonoBehaviour  {
         }*/
         var a = figure.get_figure_desc(1);
         var b = a.get_pos();
-        for( int i = 0; i < b.GetLength(0); i++)
+        for (int rot = 0; rot < 6; rot++)
         {
-            Vector3 pos = new Vector3(xpos + b[i, 0], ypos + b[i, 1], 0);
-            GameObject q = (GameObject)Instantiate(quad_prefab, pos, Quaternion.identity);
-            quads.Add(q);
+            
+
+            for (int i = 0; i < b.GetLength(0); i++)
+            {
+                Vector3 pos = new Vector3(xpos + b[i, 0]+rot*5, ypos + b[i, 1], 0);
+                GameObject q = (GameObject)Instantiate(quad_prefab, pos, Quaternion.identity);
+                //quads.Add(q);
+            }
+            b = a.left();
+            //b = a.rotate_left();
+            
         }
-        b[0, 1] = -4;
-        a.test();
+        //b[0, 1] = -4;
+        //a.test();
     }
 	
 	// Update is called once per frame
